53fc03cb377275184e5d4c090ec05a2f4117386d
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-11-24 12:39:10 +0800

    test: add test for HelloWorld component
    - Add a new file called `HelloWorld.spec.js` in the `src/components` directory
    - Import the `HelloWorld` component and the `mount` function from `@vue/test-utils`
    - Add a test for the `HelloWorld` component that checks if it renders properly
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

---
+++ src/components/HelloWorld.spec.js
@@ -0,0 +1,9 @@
+import HelloWorld from "./HelloWorld.vue";
+import { mount } from "@vue/test-utils";
+
+describe("HelloWorld", () => {
+  it("renders properly", () => {
+    const wrapper = mount(HelloWorld, { propsData: { msg: "Hello World" } });
+    expect(wrapper.text()).toContain("Hello World");
+  });
+});
