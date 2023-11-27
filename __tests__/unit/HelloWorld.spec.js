32a9a143fd4ba17fe34ac9a7eac3d26f116c05f9
stevecyj <ycsteven.tsao@gmail.com>
2023-11-28 00:02:50 +0800

    refactor: refactor test file import in HelloWorld.spec.js
    - Change the import of `mount` to `shallowMount` in the test file `HelloWorld.spec.js`
    
    Signed-off-by: stevecyj <ycsteven.tsao@gmail.com>

--- __tests__/unit/HelloWorld.spec.js
+++ __tests__/unit/HelloWorld.spec.js
@@ -1,10 +1,12 @@
 // import HelloWorld from "./HelloWorld.vue";
 import HelloWorld from "@/components/HelloWorld.vue";
-import { mount } from "@vue/test-utils";
+import { shallowMount } from "@vue/test-utils";
 
 describe("HelloWorld", () => {
   it("renders properly", () => {
-    const wrapper = mount(HelloWorld, { propsData: { msg: "Hello World" } });
+    const wrapper = shallowMount(HelloWorld, {
+      propsData: { msg: "Hello World" },
+    });
     expect(wrapper.text()).toContain("Hello World");
   });
 });
