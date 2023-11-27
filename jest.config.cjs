ed8cf9cc02641b25cb3e332ae4121f7f804c2ebd
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-11-27 10:57:26 +0800

    chore: refactor file processing options
    - Add `transformIgnorePatterns` option to ignore files in `node_modules/`
    - Remove the `transform` option for file types: `css`, `less`, `scss`, `sass`, `style`, `svg`, `png`, `jpe?g`, `ttf`, `woff2?`
    - Add `moduleNameMapper` option
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- jest.config.cjs
+++ jest.config.cjs
@@ -6,6 +6,7 @@ module.exports = {
     "^.+\\.(css|less|scss|sass|style|svg|png|jpe?g|ttf|woff2?)$":
       "jest-transform-stub",
   },
+  transformIgnorePatterns: ["/node_modules/"],
   testRegex: "(/__tests__/.*|(.|/)(test|spec)).(js|ts)$",
   moduleFileExtensions: ["vue", "js"],
   moduleNameMapper: {
