7f336fe330cc5bdb19800b37ac32faa2417216e9
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-11-28 17:03:30 +0800

    feat: refactor path resolution and scss configuration
    - Import the `normalizePath` function from the `vite` module
    - Create a `resolve` function that resolves a path relative to the current file
    - Define a variable `variablePath` that resolves to `src/variables.scss`
    - Add `additionalData` option to the `scss` preprocessor options in the `css` configuration
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
+import { normalizePath } from "vite";
+
+const resolve = (str) => normalizePath(path.resolve(__dirname, str));
+const variablePath = resolve("src/variables.scss");
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +12,13 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "${variablePath}";`,
+      },
     },
   },
 });
