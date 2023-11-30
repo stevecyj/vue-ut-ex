6f0642d4802da39ff259a3ff7820efe935abf40e
steve.tsao <steve.tsao@hlwtech.com.tw>
2023-11-30 14:18:13 +0800

    test: rename test cases for Header component
    - Change the test name from "Header 樣式發生改變時提示" to "樣式發生改變時提示"
    - Change the test name from "Header 包含 input 框" to "包含 input 框"
    - Change the test name from "Header input init value ''" to "input init value ''"
    - Change the test name from "Header input input值發生變化，數據應該跟著變" to "input值發生變化，數據應該跟著變"
    - Change the test name from "Header input 框輸入 enter，無內容時無反應" to "input 框輸入 enter，無內容時無反應"
    - Change the test name from "Header input 框輸入 enter，有內容時向外觸發事件，同時清空 inputValue" to "input 框輸入 enter，有內容時向外觸發事件，同時清空 inputValue"
    
    Signed-off-by: steve.tsao <steve.tsao@hlwtech.com.tw>

--- src/containers/TodoList/__tests__/unit/Header.spec.js
+++ src/containers/TodoList/__tests__/unit/Header.spec.js
@@ -4,24 +4,24 @@ import { shallowMount } from "@vue/test-utils";
 import { findElementByDataTest } from "@/utils";
 
 describe("Header", () => {
-  it("Header 樣式發生改變時提示", () => {
+  it("樣式發生改變時提示", () => {
     const wrapper = shallowMount(Header);
     expect(wrapper).toMatchSnapshot();
   });
 
-  it("Header 包含 input 框", () => {
+  it("包含 input 框", () => {
     const wrapper = shallowMount(Header);
     const input = findElementByDataTest(wrapper, "input")[0];
     expect(input.exists()).toBe(true);
   });
 
-  it("Header input init value ''", () => {
+  it("input init value ''", () => {
     const wrapper = shallowMount(Header);
     const inputValue = wrapper.vm.inputValue;
     expect(inputValue).toBe("");
   });
 
-  it("Header input input值發生變化，數據應該跟著變", () => {
+  it("input值發生變化，數據應該跟著變", () => {
     const wrapper = shallowMount(Header);
     const input = findElementByDataTest(wrapper, "input")[0];
     input.setValue("123");
@@ -29,7 +29,7 @@ describe("Header", () => {
     expect(inputValue).toBe("123");
   });
 
-  it("Header input 框輸入 enter，無內容時無反應", () => {
+  it("input 框輸入 enter，無內容時無反應", () => {
     const wrapper = shallowMount(Header);
     const input = findElementByDataTest(wrapper, "input")[0];
     input.setValue("");
@@ -37,7 +37,7 @@ describe("Header", () => {
     expect(wrapper.emitted().add).toBeFalsy();
   });
 
-  it("Header input 框輸入 enter，有內容時向外觸發事件，同時清空 inputValue", () => {
+  it("input 框輸入 enter，有內容時向外觸發事件，同時清空 inputValue", () => {
     const wrapper = shallowMount(Header);
     const input = findElementByDataTest(wrapper, "input")[0];
     input.setValue("123");
